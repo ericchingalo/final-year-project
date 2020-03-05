02b0425ff6fcc75f8f6cfdaefcaaf3d3b4d8c42b
chingaloEric <echingalo@gmail.com>
2020-03-05 12:09:46 +0300

    fix: 🐛 modified the analysis home page
    added the toggled side menu

--- soil-analysis-web/src/app/modules/analysis/analysis.module.ts
+++ soil-analysis-web/src/app/modules/analysis/analysis.module.ts
@@ -2,17 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 import { HomeComponent } from './pages/home/home.component';
 import { AnalysisRoutingModule } from './analysis.routes';
-import { ToolbarComponent } from './components/toolbar/toolbar.component';
 
 @NgModule({
-  declarations: [HomeComponent, ToolbarComponent],
+  declarations: [HomeComponent],
   imports: [
     CommonModule,
     MatToolbarModule,
     MatIconModule,
+    MatSidenavModule,
     AnalysisRoutingModule,
   ],
 })
