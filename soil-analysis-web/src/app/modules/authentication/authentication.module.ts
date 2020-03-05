e774422f71e7a80879df6dcf6f25aa6344a6d860
chingaloEric <echingalo@gmail.com>
2020-03-05 07:26:47 +0300

    feat: 🎸 added the login page
    added the UI for the login page.

--- soil-analysis-web/src/app/modules/authentication/authentication.module.ts
+++ soil-analysis-web/src/app/modules/authentication/authentication.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { AuthenticationRoutingModule } from './authentication.routes';
 
 @NgModule({
   declarations: [LoginComponent, RegisterComponent],
-  imports: [CommonModule, AuthenticationRoutingModule],
+  imports: [
+    CommonModule,
+    AuthenticationRoutingModule,
+    ReactiveFormsModule,
+    MatInputModule,
+  ],
 })
 export class AuthenticationModule {}
