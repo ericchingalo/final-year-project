2ea460cb373f39e04054c6739b3104f634c2da15
chingaloEric <echingalo@gmail.com>
2020-03-05 09:27:20 +0300

    feat: 🎸 added the analysis toolbar component

--- soil-analysis-web/src/app/modules/analysis/analysis.module.ts
+++ soil-analysis-web/src/app/modules/analysis/analysis.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+
 import { HomeComponent } from './pages/home/home.component';
 import { AnalysisRoutingModule } from './analysis.routes';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
 
 @NgModule({
-  declarations: [HomeComponent],
-  imports: [CommonModule, AnalysisRoutingModule],
+  declarations: [HomeComponent, ToolbarComponent],
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatIconModule,
+    AnalysisRoutingModule,
+  ],
 })
 export class AnalysisModule {}
