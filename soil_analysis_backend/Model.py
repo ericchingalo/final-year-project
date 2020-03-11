5ff9be44cf19e8e6be29d1cd131ffba934436b83
chingaloEric <echingalo@gmail.com>
2020-03-11 07:17:36 +0300

    feat: 🎸 added core codes for the app to run

---
+++ soil_analysis_backend/Model.py
@@ -0,0 +1,11 @@
+'''
+this file will hold the connections to the database
+'''
+from flask import Flask
+from marshmallow import Schema, fields, pre_load, validate
+from flask_marshmallow import Marshmallow
+from flask_sqlalchemy import SQLAlchemy
+
+
+ma = Marshmallow()
+db = SQLAlchemy()
