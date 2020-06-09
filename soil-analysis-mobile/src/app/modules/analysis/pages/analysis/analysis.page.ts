2f7a71a85144417174bf30e40908035700c795e7
chingaloEric <echingalo@gmail.com>
2020-06-09 09:34:31 +0300

    feat: 🎸 added chart service
    added a service for handling Highcharts plotting

--- soil-analysis-mobile/src/app/modules/analysis/pages/analysis/analysis.page.ts
+++ soil-analysis-mobile/src/app/modules/analysis/pages/analysis/analysis.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import * as HighCharts from 'highcharts';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { generateForm } from '../../../../shared/helpers/form-generator';
+import { AnalysisGraphService } from '../../services/analysis-graph.service';
 
 @Component({
   selector: 'app-analysis',
@@ -11,7 +12,10 @@ import { generateForm } from '../../../../shared/helpers/form-generator';
 export class AnalysisPage implements OnInit {
   analysisFormData: any;
   analysisForm: FormGroup;
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private readonly analysisGraphService: AnalysisGraphService,
+  ) {}
 
   ngOnInit() {
     this.analysisFormData = this.getFormData();
@@ -52,59 +56,11 @@ export class AnalysisPage implements OnInit {
   }
 
   onAnalysisFormSubmit(formValues: any) {
-    this.barChartPopulation();
+    console.log(formValues);
+    this.plotChart();
   }
 
-  barChartPopulation() {
-    HighCharts.chart('chart', {
-      chart: {
-        type: 'line',
-      },
-      title: {
-        text: 'Historic World PbarChartPopulationopulation by Region',
-      },
-      xAxis: {
-        categories: ['Africa', 'America', 'Asia', 'Europe', 'Oceania'],
-      },
-      yAxis: {
-        min: 0,
-        title: {
-          text: 'Population (millions)',
-          align: 'high',
-        },
-      },
-      tooltip: {
-        valueSuffix: ' millions',
-      },
-      plotOptions: {
-        bar: {
-          dataLabels: {
-            enabled: true,
-          },
-        },
-      },
-      series: [
-        {
-          type: undefined,
-          name: 'Year 1800',
-          data: [107, 31, 635, 203, 2],
-        },
-        {
-          type: undefined,
-          name: 'Year 1900',
-          data: [133, 156, 947, 408, 6],
-        },
-        {
-          type: undefined,
-          name: 'Year 2000',
-          data: [814, 841, 3714, 727, 31],
-        },
-        {
-          type: undefined,
-          name: 'Year 2016',
-          data: [1216, 1001, 4436, 738, 40],
-        },
-      ],
-    });
+  plotChart() {
+    this.analysisGraphService.plotBarChart('chart', null);
   }
 }
