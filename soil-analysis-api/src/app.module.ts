edd6d56bdce4f7ce0c12a59900b23f0364539cca
chingaloEric <echingalo@gmail.com>
2020-07-03 11:48:53 +0300

    feat: 🎸 added project modules

--- soil-analysis-api/src/app.module.ts
+++ soil-analysis-api/src/app.module.ts
@@ -3,9 +3,15 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ParameterModule } from './modules/parameter/parameter.module';
+import { UserModule } from './modules/user/user.module';
+import { PermissionModule } from './modules/permission/permission.module';
+import { DeviceModule } from './modules/device/device.module';
+import { RoleModule } from './modules/role/role.module';
+import { ResultModule } from './modules/result/result.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot()],
+  imports: [TypeOrmModule.forRoot(), ParameterModule, UserModule, PermissionModule, DeviceModule, RoleModule, ResultModule],
   controllers: [AppController],
   providers: [AppService],
 })
