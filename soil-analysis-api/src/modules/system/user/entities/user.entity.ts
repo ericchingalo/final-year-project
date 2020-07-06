48fe9cdd0b5cf5217b6a69d6d2d685577d126599
chingaloEric <echingalo@gmail.com>
2020-07-06 18:52:56 +0300

    feat: 🎸 added relationship between user and userroles

--- soil-analysis-api/src/modules/system/user/entities/user.entity.ts
+++ soil-analysis-api/src/modules/system/user/entities/user.entity.ts
@@ -1,6 +1,7 @@
-import { Entity, Column, BeforeInsert } from 'typeorm';
+import { Entity, Column, BeforeInsert, ManyToMany, JoinTable } from 'typeorm';
 import * as bcrypt from 'bcryptjs';
 import { Identifiable } from '../../../../shared/entities/identifiable.entitty';
+import { UserRole } from '../../user-role/entities/user-role.entity';
 
 @Entity('user')
 export class User extends Identifiable {
@@ -15,6 +16,10 @@ export class User extends Identifiable {
 
   @Column('text', { name: 'region', nullable: true })
   region: string;
+
+  @ManyToMany(type => UserRole)
+  @JoinTable({ name: 'roles' })
+  roles: UserRole[];
 
   @BeforeInsert()
   async hashPassword() {
