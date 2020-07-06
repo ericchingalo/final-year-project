f944e0e739678d6595abb378d07cab477c5300cf
chingaloEric <echingalo@gmail.com>
2020-07-06 19:12:14 +0300

    feat: 🎸 added relationship between device and user

--- soil-analysis-api/src/modules/device/entities/device.entity.ts
+++ soil-analysis-api/src/modules/device/entities/device.entity.ts
@@ -1,11 +1,16 @@
-import { Entity, Column, BeforeInsert } from 'typeorm';
+import { Entity, Column, BeforeInsert, OneToOne, JoinColumn } from 'typeorm';
 import * as bcrypt from 'bcryptjs';
 import { Identifiable } from '../../../shared/entities/identifiable.entitty';
+import { User } from '../../system/user/entities/user.entity';
 
 @Entity('device')
 export class Device extends Identifiable {
   @Column('text', { select: false, nullable: false })
   password: string;
+
+  @OneToOne(type => User)
+  @JoinColumn()
+  user: User;
 
   @BeforeInsert()
   async hashPassword() {
