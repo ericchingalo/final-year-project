1abecb7c24a4938cd84ed22584e2d9a1318cc5e3
chingaloEric <echingalo@gmail.com>
2020-07-07 10:10:41 +0300

    fix: 🐛 modified the validation pipe

--- soil-analysis-api/src/modules/system/user-permission/dtos/user-permission.dto.ts
+++ soil-analysis-api/src/modules/system/user-permission/dtos/user-permission.dto.ts
@@ -1,6 +1,7 @@
-import { IsString } from 'class-validator';
+import { IsString, IsNotEmpty } from 'class-validator';
 
 export class UserPermissionDTO {
   @IsString()
+  @IsNotEmpty()
   permission: string;
 }
--- soil-analysis-api/src/shared/pipes/validation.pipe.ts
+++ soil-analysis-api/src/shared/pipes/validation.pipe.ts
@@ -2,12 +2,21 @@ import {
   PipeTransform,
   ArgumentMetadata,
   BadRequestException,
+  HttpException,
+  HttpStatus,
 } from '@nestjs/common';
 
 import { validate } from 'class-validator';
 import { plainToClass } from 'class-transformer';
 export class CustomValidationPipe implements PipeTransform<any> {
   async transform(value: any, { metatype }: ArgumentMetadata) {
+    if (value instanceof Object && this.isEmpty(value)) {
+      throw new HttpException(
+        'Validation failed: No body submitted',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
     if (!metatype || !this.toValidate(metatype)) {
       return value;
     }
@@ -15,18 +24,36 @@ export class CustomValidationPipe implements PipeTransform<any> {
     const object = plainToClass(metatype, value);
     const error = await validate(object);
 
-    console.error(error);
-
     if (error.length > 0) {
-      throw new BadRequestException('Validation Failed');
+      throw new BadRequestException(
+        `Validation failed: ${this.formatErrors(error)}`,
+      );
     }
 
     return value;
   }
 
+  private formatErrors(errors: any[]) {
+    return errors
+      .map(error => {
+        const { constraints } = error;
+
+        for (const property in constraints) {
+          if (constraints.hasOwnProperty(property)) {
+            return constraints[property];
+          }
+        }
+      })
+      .join(', ');
+  }
+
   //   helper to check native Javascript types
-  private toValidate(metatype: any): boolean {
-    const types: any[] = [String, Boolean, Number, Array, Object];
+  private toValidate(metatype: Function): boolean {
+    const types: Function[] = [String, Boolean, Number, Array, Object];
     return !types.includes(metatype);
+  }
+
+  private isEmpty(value: any) {
+    return Object.keys(value).length === 0;
   }
 }
