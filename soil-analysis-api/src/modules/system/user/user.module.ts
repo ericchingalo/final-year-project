e50f7a4a0773b762f9545172d254c59a96781fc6
chingaloEric <echingalo@gmail.com>
2020-07-21 13:11:11 +0300

    fix: 🐛 registered auth service

--- soil-analysis-api/src/modules/system/user/user.module.ts
+++ soil-analysis-api/src/modules/system/user/user.module.ts
@@ -4,10 +4,11 @@ import { User } from './entities/user.entity';
 import { UserService } from './services/user.service';
 import { UserController } from './controller/user.controller';
 import { UserRoleModule } from '../user-role/user-role.module';
+import { AuthService } from './services/auth.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User]), UserRoleModule],
-  providers: [UserService],
+  providers: [UserService, AuthService],
   exports: [UserService],
   controllers: [UserController],
 })
