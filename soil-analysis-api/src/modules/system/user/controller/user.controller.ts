40690a187f3ddf6dabd55633d339f7a79ce58a2f
chingaloEric <echingalo@gmail.com>
2020-07-21 13:10:32 +0300

    feat: 🎸 added login endpoint

--- soil-analysis-api/src/modules/system/user/controller/user.controller.ts
+++ soil-analysis-api/src/modules/system/user/controller/user.controller.ts
@@ -3,10 +3,19 @@ import { BaseController } from 'src/shared/controllers/base.controller';
 import { UserDTO } from '../dtos/user.dto';
 import { User } from '../entities/user.entity';
 import { UserService } from '../services/user.service';
+import { UserLoginDTO } from '../dtos/user-login.dto';
+import { AuthService } from '../services/auth.service';
 
 @Controller('users')
 export class UserController extends BaseController<User, UserDTO, UserDTO> {
-  constructor(private userService: UserService) {
+  constructor(
+    private userService: UserService,
+    private authService: AuthService,
+  ) {
     super(userService);
+  }
+
+  async login(user: UserLoginDTO): Promise<any> {
+    return await this.authService.login(user.username, user.password);
   }
 }
