7cec3c26ee4a8ce6b495056cd2217c25815bd887
chingaloEric <echingalo@gmail.com>
2020-07-21 16:20:57 +0300

    feat: 🎸 added database migrations

---
+++ soil-analysis-api/src/migrations/1595337531724-User.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class User1595337531724 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
