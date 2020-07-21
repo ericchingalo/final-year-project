2f861bca07bf9945da5498055b86716acc1c2453
chingaloEric <echingalo@gmail.com>
2020-07-21 23:35:24 +0300

    fix: 🐛 added migration script

--- soil-analysis-api/src/app.controller.ts
+++ soil-analysis-api/src/app.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
+import * as bcrypt from 'bcryptjs';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
