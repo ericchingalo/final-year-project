f9634e34ce7ffba7a0f9ebf7b9ed47a477bc234a
chingaloEric <echingalo@gmail.com>
2020-07-24 01:42:36 +0300

    feat: 🎸 addde a side-nav

--- soil-analysis-web/src/app/components/home/home.component.ts
+++ soil-analysis-web/src/app/components/home/home.component.ts
@@ -3,13 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  user: {
+    username: string;
+    role: string;
+  };
+  constructor() {}
 
   ngOnInit() {
+    this.user = {
+      username: 'Chingalo',
+      role: 'Admin',
+    };
   }
-
 }
