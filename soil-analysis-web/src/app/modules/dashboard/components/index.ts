69d62ec11748c3e1e2c90bee50c5b72d12e0f452
chingaloEric <echingalo@gmail.com>
2020-07-27 19:40:34 +0300

    feat: 🎸 added data-table

---
+++ soil-analysis-web/src/app/modules/dashboard/components/index.ts
@@ -0,0 +1,3 @@
+import { DataTableComponent } from './data-table/data-table.component';
+
+export const components: any[] = [DataTableComponent];
--- soil-analysis-web/src/app/modules/dashboard/dashboard.module.ts
+++ soil-analysis-web/src/app/modules/dashboard/dashboard.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardRoutingModule } from './dashboard.routes';
 import { pages } from './pages';
+import { components } from './components';
+import { MaterialModule } from '../../core/material/material.module';
 
 @NgModule({
-  declarations: [...pages],
-  imports: [CommonModule, DashboardRoutingModule],
+  declarations: [...pages, ...components],
+  imports: [CommonModule, DashboardRoutingModule, MaterialModule],
 })
 export class DashboardModule {}
