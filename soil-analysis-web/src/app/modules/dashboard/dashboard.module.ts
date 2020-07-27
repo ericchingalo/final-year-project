e4840877ecc48c57dbb42f8c84a5c3d18dacf434
chingaloEric <echingalo@gmail.com>
2020-07-27 19:44:06 +0300

    feat: 🎸 added parameter pipes

--- soil-analysis-web/src/app/modules/dashboard/dashboard.module.ts
+++ soil-analysis-web/src/app/modules/dashboard/dashboard.module.ts
@@ -4,9 +4,10 @@ import { DashboardRoutingModule } from './dashboard.routes';
 import { pages } from './pages';
 import { components } from './components';
 import { MaterialModule } from '../../core/material/material.module';
+import { pipes } from './pipes';
 
 @NgModule({
-  declarations: [...pages, ...components],
+  declarations: [...pages, ...components, ...pipes],
   imports: [CommonModule, DashboardRoutingModule, MaterialModule],
 })
 export class DashboardModule {}
---
+++ soil-analysis-web/src/app/modules/dashboard/pipes/index.ts
@@ -0,0 +1,3 @@
+import { ParameterPipe } from './parameter.pipe';
+
+export const pipes: any[] = [ParameterPipe];
