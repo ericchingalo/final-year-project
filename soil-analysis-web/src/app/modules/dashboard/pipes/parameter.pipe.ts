d239a9e34a1f1ff4f150c7e518fb302477390040
chingaloEric <echingalo@gmail.com>
2020-07-27 22:29:35 +0300

    feat: 🎸 added pipe for transforming parameter values

--- soil-analysis-web/src/app/modules/dashboard/pipes/parameter.pipe.ts
+++ soil-analysis-web/src/app/modules/dashboard/pipes/parameter.pipe.ts
@@ -1,12 +1,17 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { ParameterResult } from '../models/parameter-result.model';
+import * as _ from 'lodash';
 
 @Pipe({
-  name: 'parameter'
+  name: 'parameter',
 })
 export class ParameterPipe implements PipeTransform {
+  transform(values: ParameterResult[], parameter): number | string {
+    const paramObject: ParameterResult = _.find(
+      values,
+      (value: ParameterResult) => value.parameter === parameter
+    );
 
-  transform(value: any, ...args: any[]): any {
-    return null;
+    return paramObject ? paramObject.value : 'N/A';
   }
-
 }
