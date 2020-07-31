6cd07343d40cf96d05f695aa1033dbe1e14af8b0
chingaloEric <echingalo@gmail.com>
2020-07-31 15:52:44 +0300

    feat: 🎸 added snackBar service

--- soil-analysis-web/src/app/shared/shared.module.ts
+++ soil-analysis-web/src/app/shared/shared.module.ts
@@ -1,12 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { MaterialModule } from '../core/material/material.module';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule, MaterialModule],
 })
-export class SharedModule { }
+export class SharedModule {}
