8ebd75a1e1e869a7644a990904136ba6ee126d17
chingaloEric <echingalo@gmail.com>
2020-07-31 15:34:18 +0300

    feat: 🎸 added login page

--- soil-analysis-web/src/app/authentication/authentication.module.ts
+++ soil-analysis-web/src/app/authentication/authentication.module.ts
@@ -3,9 +3,16 @@ import { CommonModule } from '@angular/common';
 import { AuthenticationRoutingModule } from './authentication.routes';
 import { LoginComponent } from './pages/login/login.component';
 import { pages } from './pages';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MaterialModule } from '../core/material/material.module';
 
 @NgModule({
   declarations: [...pages],
-  imports: [CommonModule, AuthenticationRoutingModule],
+  imports: [
+    CommonModule,
+    AuthenticationRoutingModule,
+    ReactiveFormsModule,
+    MaterialModule,
+  ],
 })
 export class AuthenticationModule {}
