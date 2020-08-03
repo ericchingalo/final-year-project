f0eaf3b370c435ffe5b58a5c511b304780f6ad93
chingaloEric <echingalo@gmail.com>
2020-08-03 18:12:40 +0300

    fix: 🐛 extended base service on user service

--- soil-analysis-web/src/app/modules/user/services/user.service.ts
+++ soil-analysis-web/src/app/modules/user/services/user.service.ts
@@ -1,11 +1,18 @@
 import { Injectable } from '@angular/core';
 import { User } from '../models/user.model';
+import { BaseService } from '../../../shared/services/base.service';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
-export class UserService {
-  constructor() {}
+export class UserService extends BaseService<User> {
+  constructor(
+    private readonly httpService: HttpClient,
+    public endnpoint = 'users'
+  ) {
+    super(httpService, endnpoint);
+  }
 
   getDummyUsers(): User[] {
     return [
