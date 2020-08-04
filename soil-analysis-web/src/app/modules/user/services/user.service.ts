1268d424c9704cbac1a37bf9468cc7e6b6e14fc1
chingaloEric <echingalo@gmail.com>
2020-08-04 13:59:23 +0300

    feat: 🎸 added user service

--- soil-analysis-web/src/app/modules/user/services/user.service.ts
+++ soil-analysis-web/src/app/modules/user/services/user.service.ts
@@ -1,11 +1,31 @@
 import { Injectable } from '@angular/core';
 import { User } from '../models/user.model';
 import { BaseService } from '../../../shared/services/base.service';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
+  endpoint: 'users';
+  constructor(private http: BaseService<User>) {}
+
+  findAll(): Observable<any> {
+    return this.http.findAll(this.endpoint);
+  }
+
+  create(user: User): Observable<any> {
+    return this.http.create(this.endpoint, user);
+  }
+
+  update(id: string, updatedUser: Partial<User>): Observable<any> {
+    return this.http.update(this.endpoint, id, updatedUser);
+  }
+
+  delete(id: string): Observable<any> {
+    return this.http.delete(this.endpoint, id);
+  }
+
   getDummyUsers(): User[] {
     return [
       {
