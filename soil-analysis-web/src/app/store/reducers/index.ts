4d3356277fb95e684ac48c67d6e8871e39ae68ac
chingaloEric <echingalo@gmail.com>
2020-08-04 11:30:21 +0300

    fix: 🐛 registered the device reducer

--- soil-analysis-web/src/app/store/reducers/index.ts
+++ soil-analysis-web/src/app/store/reducers/index.ts
@@ -2,13 +2,17 @@ import { ActionReducerMap, MetaReducer } from '@ngrx/store';
 import { environment } from '../../../environments/environment';
 import { UsersState } from '../state/users.state';
 import { UsersReducer } from './users.reducers';
+import { DevicesState } from '../state/devices.state';
+import { DevicesReducer } from './devices.reducers';
 
 export interface State {
   users: UsersState;
+  devices: DevicesState;
 }
 
 export const reducers: ActionReducerMap<State> = {
   users: UsersReducer,
+  devices: DevicesReducer,
 };
 
 export const metaReducers: MetaReducer<State>[] = !environment.production
