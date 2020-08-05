b37b2cf4d95f7bb63fe1db94d45fe47a838a8190
chingaloEric <echingalo@gmail.com>
2020-08-05 16:47:09 +0300

    fix: 🐛 loaded results

--- soil-analysis-web/src/app/modules/dashboard/pages/dashboard-home/dashboard-home.component.ts
+++ soil-analysis-web/src/app/modules/dashboard/pages/dashboard-home/dashboard-home.component.ts
@@ -4,6 +4,9 @@ import { Result } from '../../models/results.model';
 import { ResultsService } from '../../services/results.service';
 import { RegionDataCount } from '../../models/region-data-count.model';
 import { FilterMetadata } from '../../models/filter-metadata.model';
+import { Store } from '@ngrx/store';
+import { State } from 'src/app/store/reducers';
+import { loadResults } from '../../store/actions/results.actions';
 
 @Component({
   selector: 'app-dashboard-home',
@@ -15,12 +18,16 @@ export class DashboardHomeComponent implements OnInit {
   regionDataCount: RegionDataCount[];
   showCustomGraph: boolean;
   customGraphConfig: FilterMetadata;
-  constructor(private readonly resultsService: ResultsService) {}
+  constructor(
+    private readonly resultsService: ResultsService,
+    private store: Store<State>
+  ) {}
 
   ngOnInit() {
     this.results = this.resultsService.getResults();
     this.resizeWindow();
     this.showCustomGraph = false;
+    this.store.dispatch(loadResults());
   }
 
   resizeWindow() {
