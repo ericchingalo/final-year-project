50e796b6bb8c693a4e23398517219c4d546800ce
chingaloEric <echingalo@gmail.com>
2020-08-19 18:24:40 +0300

    feat: 🎸 allowed editing of user

--- soil-analysis-mobile/src/app/modules/account/pages/edit-account/edit-account.page.ts
+++ soil-analysis-mobile/src/app/modules/account/pages/edit-account/edit-account.page.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { State } from '../../../../store/reducers/index';
+import { generateForm } from '../../../../shared/helpers/form-generator';
+import { User } from '../../models/user.model';
+import { getCurrentUser } from '../../../../store/selectors/current-user.selectors';
+import { take } from 'rxjs/operators';
+import { editUser } from '../../../../store/actions/current-user.actions';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-edit-account',
@@ -7,10 +16,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditAccountPage implements OnInit {
   defaultHref: string;
+  editFormGroup: FormGroup;
+  editFormData: any;
+  currentUser: User;
 
-  constructor() {}
+  constructor(
+    private fb: FormBuilder,
+    private store: Store<State>,
+    private router: Router,
+  ) {}
 
   ngOnInit() {
     this.defaultHref = '/tabs/account';
+    this.getCurrentUser();
+    this.editFormData = this.getEditFormData();
+    this.setEditForm();
+  }
+
+  getCurrentUser() {
+    this.store
+      .select(getCurrentUser)
+      .pipe(take(1))
+      .subscribe((user) => (this.currentUser = user));
+  }
+
+  setEditForm() {
+    this.editFormGroup = generateForm(
+      this.editFormGroup,
+      this.editFormData,
+      this.fb,
+      this.currentUser,
+    );
+  }
+
+  getEditFormData() {
+    return {
+      inputs: [
+        {
+          type: 'text',
+          required: true,
+          formControlName: 'username',
+          label: 'User Name',
+        },
+      ],
+    };
+  }
+
+  onUpdateUser() {
+    const user = { username: this.editFormGroup.value.username };
+    this.store.dispatch(editUser({ user }));
+    this.router.navigate(['tabs/account']);
   }
 }
